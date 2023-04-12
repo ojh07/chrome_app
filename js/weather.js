bfb99e6a723ee808ab1317fc491bd0c54c33a913
ojh07 <jonghyuck134@gmail.com>
2023-04-12 17:39:29 +0900

    chrome_app_making

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "8f20324d53b59e33c5590eac462ee9f8";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
